8d41479379e6575c8ef8deb1b0f10a29630fcb34
Alex Nypels <nypels.alexandre@outlook.com>
2022-02-20 21:48:22 +0000

    Load posts on home page and fetch data. Format post date.

--- flask_blog/main/routes.py
+++ flask_blog/main/routes.py
@@ -4,6 +4,9 @@ from flask_blog.users.forms import SettingsForm
 from flask_blog.users.utils import validate_settings
 from flask import redirect, request, render_template, url_for
 from flask_login import login_user, current_user, logout_user, login_required, LoginManager
+from bson import ObjectId
+from datetime import datetime
+from flask_blog.main.utils import format_post_date
 
 main = Blueprint("main", __name__)
 
@@ -11,10 +14,25 @@ main = Blueprint("main", __name__)
 @main.route("/", methods=["GET", "POST"])
 @main.route("/home", methods=["GET", "POST"])
 def home():
+    ##########################################
     settingsForm = SettingsForm()
     if settingsForm.validate_on_submit():
         validate_settings(settingsForm)
-
+    ##########################################
+    
+    # Get post from DB
+    posts = mongo.db.posts.find().sort("posted_date", -1)
+    updated_post = []
+    for post in posts:
+        author = mongo.db.users.find_one(ObjectId(post["author"]))
+        category = mongo.db.categories.find_one(ObjectId(post["category"]))
+        post["author"] = author
+        post["category"] = category
+        newDate = format_post_date(post["posted_date"])
+        post["posted_date"] = newDate
+        
+        updated_post.append(post)
+        
     return render_template("home.html",
                            page_title="Home Page", active_link="home", 
-                           settingsForm=settingsForm)
+                           settingsForm=settingsForm, posts=updated_post)
