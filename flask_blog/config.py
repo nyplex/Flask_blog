e7f356336cacd204842b90a6cd15529f66aec598
Alex Nypels <nypels.alexandre@outlook.com>
2022-04-14 09:12:05 +0100

    Add env variables to Heroku

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -5,9 +5,3 @@ class Config:
     MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
     MONGO_URI = os.environ.get("MONGO_URI")
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    # MAIL_SERVER =  'smtp.gmail.com'
-    # MAIL_PORT = 465
-    # MAIL_USE_TLS = False
-    # MAIL_USE_SSL = True
-    # MAIL_USERNAME = os.environ.get('EMAIL_USER'),
-    # MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
